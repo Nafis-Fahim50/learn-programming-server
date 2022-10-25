7f156b112d5166ca600a08901ba7b40b3ecc5f09
Nafis-Fahim50 <nafisf96@gmail.com>
2022-10-25 17:10:06 +0600

    create fake dataset

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ app.use (cors());
 
 const port = process.env.port || 5000;
 
+const courses = require('./Data/courses.json')
+
 app.get('/',(req,res)=>{
     res.send('Server is running on this port');
+})
+
+app.get('/courses',(req,res)=>{
+    res.send(courses);
 })
 
 app.listen(port, ()=>{
